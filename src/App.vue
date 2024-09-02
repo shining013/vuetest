a1698fa841e7979e1e8c08fef27baa2e6d129bb7
shining013 <nevertheless113@naver.com>
2024-09-02 17:37:30 +0900

    feat: 배열

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <template>
   <div class="container secWrap">
-    <img src="./assets/images/Escape.jpg" alt="" />
-    <div class="card">
-      <h1>{{ title }}</h1>
-      <p>🍿 {{ num }}</p>
-      <p>대여 :{{ won }}</p>
+    <h3>HOT & NEW</h3>
+    <div class="cardWrap">
+      <div class="card" v-for="(item, i) in data_m" v-bind:key="i">
+        <div class="imgWrap">
+          <img v-bind:src="`./images/${item.img}`" alt="" />
+        </div>
+        <div class="cardText">
+          <h1>{{ item.title }}</h1>
+          <p>🍿 {{ item.num }}</p>
+          <p>대여 :{{ item.won }}</p>
+        </div>
+      </div>
     </div>
-    
-    {{ todos[0] }}
-    <div v-for="(item, index) in todos" v-bind:key="index">{{ item }}</div>
+  </div>
+
+  <!-- <div v-for="(item, index) in todos" v-bind:key="index">{{ item }}</div>
 
     <ul>
       <li v-for="(item, i) in todos" :key="i">{{ item }}</li>
-    </ul>
-  </div>
+    </ul> -->
 </template>
 
 <script>
@@ -21,10 +27,16 @@ export default {
   name: "App",
   data() {
     return {
-      title: "탈주",
-      num: "8.1",
-      won: "11,000",
-      todos: ["구교", "이제훈", "이옥섭"],
+      data_m: [
+        { title: "탈주", num: "8.1", won: "11,000", img: "Escape.jpg" },
+        {
+          title: "악은 존재하지 않는다",
+          num: "8.0",
+          won: "10,890",
+          img: "evil.jpg",
+        },
+        { title: "파묘", num: "7.8", won: "9,900", img: "dead.jpg" },
+      ],
     };
   },
 };
@@ -32,4 +44,35 @@ export default {
 
 <style lang="scss" scoped>
 @import "./assets/css/style.scss";
+.secWrap {
+  padding: 5px 16px;
+  background-color: #fff;
+}
+.cardWrap {
+  display: flex;
+  gap: 10px;
+  .card {
+    width: 212px;
+    img {
+      width: 100%;
+    }
+    .imgWrap {
+      width: 100%;
+      overflow: hidden;
+      border-radius: 10px;
+      img {
+        display: block;
+        width: 100%;
+        transition: 0.3s;
+      }
+      &:hover img {
+        transform: scale(1.05);
+      }
+    }
+    .cardText {
+      padding: 5px;
+      font-size: 12px;
+    }
+  }
+}
 </style>
